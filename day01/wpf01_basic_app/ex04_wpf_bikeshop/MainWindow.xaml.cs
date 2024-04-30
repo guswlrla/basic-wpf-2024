00f456d5cc160550fe205c9e63cae25e0165e666
guswlrla <rlaguswl_01@naver.com>
2024-04-30 17:09:23 +0900

    update

--- day01/wpf01_basic_app/ex04_wpf_bikeshop/MainWindow.xaml.cs
+++ day01/wpf01_basic_app/ex04_wpf_bikeshop/MainWindow.xaml.cs
@@ -26,7 +26,7 @@ namespace ex04_wpf_bikeshop
         {
             // 코드비하인드에서 Source 속성에 페이지를 넣을 때는
             // UriKind.RelativeOrAbsolute 파라미터를 넣어야!
-            MainFrame.Source = new Uri("/ContactPage.xaml", UriKind.RelativeOrAbsolute);
+            MainFrame.Source = new Uri("/MenuPage.xaml", UriKind.RelativeOrAbsolute);
             Debug.WriteLine("Debug!");
         }
     }
