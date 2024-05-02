49599678634d4840a32f0b7c1caf3f790d98515c
guswlrla <rlaguswl_01@naver.com>
2024-05-02 17:17:04 +0900

    update

---
+++ day03/wpf03_basic_app/ex05_wpf_bikeshop/MainWindow.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ex05_wpf_bikeshop
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            // 객체 생성시 프로퍼티(변수) 값을 설정하는 방법1
+            Bike myBike = new Bike() { Speed = 60, Color = Colors.Gray };
+
+            // 객체 생성시 속성초기화, 방법2
+            Bike yourBike = new Bike();
+            yourBike.Speed = 50;
+            yourBike.Color = Colors.Gray; // Color.FromArgb(255, 255, 0, 0);
+
+            StsBike.DataContext = yourBike; // WPF방식
+
+            TxtMyBikeSpeed.Text = myBike.Speed.ToString(); // 전통적인 윈폼방식
+
+
+        }
+
+        private void TxtMyBikeSpeed_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Enter) 
+            {
+                TxtCopySpeed.Text = TxtMyBikeSpeed.Text;
+            }
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("버튼클릭");
+        }
+    }
+}
---
+++ day03/wpf04_mvvm_app/ex07_EmployeeMngApp/Models/Employees.cs
@@ -0,0 +1,36 @@
+﻿namespace ex07_EmployeeMngApp.Models
+{
+    class Employees
+    {
+        public int Id { get; set; }
+        public int EmpName { get; set; }
+        public decimal Salary { get; set; }
+        public string DeptName { get; set; }
+        public string Addr { get; set; }
+
+        public static readonly string SELECT_QUERY = @"SELECT [Id]
+                                                             ,[EmpName]
+                                                             ,[Salary]
+                                                             ,[DeptName]
+                                                             ,[Addr]
+                                                         FROM [dbo].[Employees]";
+        public static readonly string INSERT_QUERY = @"INSERT INTO [dbo].[Employees]
+                                                                  ([EmpName]
+                                                                  ,[Salary]
+                                                                  ,[DeptName]
+                                                                  ,[Addr])
+                                                            VALUES
+                                                                  (@EmpName
+                                                                  ,@Salary
+                                                                  ,@DeptName
+                                                                  ,@Addr)";
+        public static readonly string UPDATE_QUERY = @"UPDATE [dbo].[Employees]
+                                                          SET [EmpName] = @EmpName
+                                                             ,[Salary] = @Salary
+                                                             ,[DeptName] = @DeptName
+                                                             ,[Addr] = @Addr
+                                                        WHERE Id = @Id";
+        public static readonly string DELETE_QUERY = @"DELETE FROM [dbo].[Employees]
+                                                             WHERE Id=@Id";
+    }
+}
