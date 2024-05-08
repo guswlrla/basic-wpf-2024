945d83ec703580f21a10a686944e55efeb60cff2
guswlrla <rlaguswl_01@naver.com>
2024-05-08 17:19:51 +0900

    update

--- day05/wpf05_mvvm_app/ex07_EmployeeMngApp/Helpers/Common.cs
+++ day05/wpf05_mvvm_app/ex07_EmployeeMngApp/Helpers/Common.cs
@@ -2,14 +2,14 @@
 
 namespace ex07_EmployeeMngApp.Helpers
 {
-    class Common
+    public class Common
     {
         public static readonly string CONNSTRING = "Data Source=localhost;" +
                                                     "Initial Catalog=EMS;" +
                                                     "Persist Security Info=True;" +
                                                     "User ID=ems_user;" +
                                                     "Encrypt=False;" +
-                                                    "Password=ems_p@ss";
+                                                    "Password=ems_p@ss;";
         // Metro방식의 다이얼로그 적용
         public static IDialogCoordinator DialogCoordinator { get; set; }
     }
